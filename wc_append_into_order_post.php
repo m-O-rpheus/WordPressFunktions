26ac758bd2c0b643187d4d88b906f9cfa07d3b78
m0rph3u <73857179+m0rph3u@users.noreply.github.com>
2021-12-17 10:24:24 +0100

    Create wc_append_into_order_post.php

---
+++ wc_append_into_order_post.php
@@ -0,0 +1,19 @@
+<?php
+
+
+    // Hinzufügen einer Meta Box unter einer Bestellung
+
+    add_action( 'add_meta_boxes', function() {
+
+        add_meta_box( 'mj_custom_field_metabox', __('Meine Spalte', 'mj_custom_field'), 'mj_custom_field_metabox_append', 'shop_order', 'side', 'high' );
+    }, 10, 0 );
+
+    function mj_custom_field_metabox_append() {
+
+        global $post;
+
+        echo '<a href="#" target="_blank">Custom Link: ' . $post->ID . '</a>';
+    }
+
+
+?>
