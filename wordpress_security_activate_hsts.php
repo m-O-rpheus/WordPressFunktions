525ff6256fc4bc3ea0b2513d897a5ad7e4a2f8ba
Markus Jäger <73857179+m-O-rpheus@users.noreply.github.com>
2023-06-20 14:31:32 +0200

    Update wordpress_security_activate_hsts.php

--- wordpress_security_activate_hsts.php
+++ wordpress_security_activate_hsts.php
@@ -1,15 +1,21 @@
 <?php
 
 
-//wp-config.php
+// wp-config.php
     define('FORCE_SSL', true);
 
 
 
-//functions.php
+// entweder functions.php - Betrifft nicht alle Dateien z.B. keine Bilder.
     add_action('send_headers', function() {
         header('Strict-Transport-Security: max-age=31536000; includeSubDomains; preload');
     });
+
+
+// oder htaccess - Betrifft alle Dateien inkl. Bilder.
+    <IfModule mod_headers.c>
+        Header set Strict-Transport-Security "max-age=31536000; includeSubDomains; preload"
+    </IfModule>
 
 
 ?>
